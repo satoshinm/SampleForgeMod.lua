d2958cc0b5e06be4c200e61612e19939dd4dff8e
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-07-14 23:12:14 -0700

    Demonstrate iterating and logging client mods through cForgeMods

--- main.lua
+++ main.lua
@@ -13,7 +13,15 @@ end
 
 function OnLoginForge(Client)
     LOG("SampleForgeMod received forge mods connection")
-    -- TODO: Client:GetForgeMods()
+    local mods = Client:GetForgeMods()
+    LOG("SampleForgeMod got mods: " .. mods:GetNumMods())
+
+    for i = 1, mods:GetNumMods() do
+        local name = mods:GetModNameAt(i - 1)
+        local version = mods:GetModVersionAt(i - 1)
+
+        LOG("SampleForgeMod mod #" .. i .. " is " .. name .. " version " .. version)
+    end
 
     return false -- Allow all connections
 end
