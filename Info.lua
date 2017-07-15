43e24669aed83664d92df425cec2988ecc54a555
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-07-14 19:34:24 -0700

    Initial commit

---
+++ Info.lua
@@ -0,0 +1,11 @@
+g_PluginInfo =
+{
+    Name = "SampleForgeMod",
+    Date = "2017-07-14",
+    Description = "This is an example plugin demonstrating a Forge mod",
+
+    AdditionalInfo = {},
+    Commands = {},
+    ConsoleCommands = {},
+    Permissions = {},
+}
---
+++ main.lua
@@ -0,0 +1,7 @@
+function Initialize(Plugin)
+    Plugin:SetName("SampleForgeMod")
+    Plugin:SetVersion(1)
+
+    LOG("Initializing SampleForgeMod!")
+    return true
+end
