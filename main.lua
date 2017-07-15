873f01b13a75377083ccfa632b7dd14f5d7c36fc
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-07-14 20:52:31 -0700

    Comment out Mods logging since it isn't bridged correctly: Plugins/SampleForgeMod/main.lua:16: attempt to concatenate local 'Mods' (a boolean value)

--- main.lua
+++ main.lua
@@ -2,6 +2,18 @@ function Initialize(Plugin)
     Plugin:SetName("SampleForgeMod")
     Plugin:SetVersion(1)
 
-    LOG("Initializing SampleForgeMod!")
+    LOG("Initializing SampleForgeMod...")
+
+    cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_FORGE_MODS, OnPlayerForgeMods)
+    
+    LOG("Initialized SampleForgeMod!")
+
     return true
+end
+
+function OnPlayerForgeMods(Client, Mods)
+    LOG("SampleForgeMod received forge mods connection")
+    -- LOG("Mods: " .. Mods)
+
+    return false -- Allow all connections
 end
